6816315691da0867d67dfb7b22411699f4fba845
Ivan Burzakovkiy <burzakovskiy.ivan@gmail.com>
2019-08-13 17:50:54 +0300

    Added Initial folder existence validation

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+
 const INITIAL_FOLDER_PATH = "C:/For_testing";
+const REPORT_FILE_PATH = `${INITIAL_FOLDER_PATH}/Report.txt`;
+
+let reportStream;
 
 function validateAnswer(answer) {
     if(answer !== "done") {
@@ -10,8 +15,38 @@ function validateAnswer(answer) {
 }
 
 // The main function
-function startTesting() {
+async function startTesting() {
     console.info("--- Testing is started... ---");
+    await createReportFile();
+    if(await !checkInitialFolderExistence()) {
+        return
+    }
+}
+
+async function createReportFile() {
+    reportStream = await fs.createWriteStream(REPORT_FILE_PATH);
+    await addLogToReportFile("--- Testing is running... ---");
+}
+
+function addLogToReportFile(text) {
+    reportStream.write(`${returnCurrentDate()} ${text}\n`);
+}
+
+function returnCurrentDate() {
+    return new Date().toISOString();
+}
+
+async function checkInitialFolderExistence() {
+    await addLogToReportFile(`--- Checking the folder ${INITIAL_FOLDER_PATH} existence... ---`);
+    fs.readdir(INITIAL_FOLDER_PATH, (err) => {
+        if(err) {
+            addLogToReportFile(`*** ERROR **** The folder ${INITIAL_FOLDER_PATH} doesn\'t exist. Testing is stopped!!! ***`);
+            return false;
+        } else {
+            addLogToReportFile(`--- The folder ${INITIAL_FOLDER_PATH} exists... ---`);
+            return true;
+        };
+    });
 }
 
 module.exports = { validateAnswer, startTesting };
