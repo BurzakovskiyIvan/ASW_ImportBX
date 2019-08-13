1943c8e8822eb2e9912a15711b51919c50056e97
Ivan Burzakovkiy <burzakovskiy.ivan@gmail.com>
2019-08-13 12:27:24 +0300

    Creation of the initial command
    After the app is started the first ask is displayed ("Creation of the initial folder C:/For_testing")

---
+++ index.js
@@ -0,0 +1,27 @@
+const program = require('commander');
+const { prompt } = require('inquirer');
+
+const questions = [
+    {
+      type : 'input',
+      name : 'initialFolderCreation',
+      message : '*** Please, create a folder "C:\\For_testing" and import the whole unzipped suite to there. After that type "done" here. ***'
+    }
+];
+
+program
+    .version('0.0.1')
+    .description('Import promotions BX file');
+
+program
+    .command('start-testing')
+    .alias('start')
+    .description('Check whether files were imported into C:\\For_testing')
+    .action(() => {
+        prompt(questions)
+            .then((answers) => {
+                console.log(answers);          
+            });
+    });
+    
+program.parse(process.argv);
