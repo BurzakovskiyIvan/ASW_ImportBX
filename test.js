8b821df8710549e436d2815c2e2b70f26f121036
Ivan Burzakovkiy <burzakovskiy.ivan@gmail.com>
2019-08-15 17:09:32 +0300

    Output of initialFolder content + bug fixing
    Output of initialFolder content has been added + bug fixing

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 
-const INITIAL_FOLDER_PATH = "C:/For_testing";
+const INITIAL_FOLDER_PATH = "C:\\For_testing";
 const REPORT_FILE_PATH = `${INITIAL_FOLDER_PATH}/Report.txt`;
+const DEST_FOLDER_PATH_64 = "C:\\Program Files (x86)\\StoreLine\\Office\\Import";
+const DEST_FOLDER_PATH_32 = "C:\\Program Files\\StoreLine\\Office\\Import";
 
-let reportStream;
+let reportStream,
+    initialFolderContent = [];
 
 function validateAnswer(answer) {
     if(answer !== "done") {
@@ -17,36 +20,84 @@ function validateAnswer(answer) {
 // The main function
 async function startTesting() {
     console.info("--- Testing is started... ---");
-    await createReportFile();
-    if(await !checkInitialFolderExistence()) {
-        return
+
+    let initialFolderExists = await checkInitialFolderExistence();
+    if(!initialFolderExists) return;
+
+    let initialFolderIsNotEmpty = await checkInitialFolderContent();
+    if(!initialFolderIsNotEmpty) return;
+
+    await showInitialFolderContent();
+
+    /* let truePathToImportFolder = await searchForStorelineImportFolder();
+    if(!truePathToImportFolder) return; */
+
+
+}
+
+function checkInitialFolderExistence() {
+    console.log(`--- Checking the folder ${INITIAL_FOLDER_PATH} existence... ---`);
+    
+    try {
+        initialFolderContent = fs.readdirSync(INITIAL_FOLDER_PATH);
+        createReportFile();
+        addLogToReportFile(`--- The folder ${INITIAL_FOLDER_PATH} exists... ---`);
+        return true;
+    }
+    catch(err) {
+        console.log(`*** ERROR **** The folder ${INITIAL_FOLDER_PATH} doesn\'t exist. Testing is stopped!!! ***`);
+        return false;
     }
 }
 
-async function createReportFile() {
-    reportStream = await fs.createWriteStream(REPORT_FILE_PATH);
-    await addLogToReportFile("--- Testing is running... ---");
+function createReportFile() {
+    reportStream = fs.createWriteStream(REPORT_FILE_PATH, {flags: 'a'});
+    reportStream.write(`${returnCurrentDate()} --- Testing is started... ---\r\n`);
 }
 
 function addLogToReportFile(text) {
-    reportStream.write(`${returnCurrentDate()} ${text}\n`);
+    reportStream.write(`${returnCurrentDate()} ${text}\r\n`);
+    console.log(text);
 }
 
 function returnCurrentDate() {
     return new Date().toISOString();
 }
 
-async function checkInitialFolderExistence() {
-    await addLogToReportFile(`--- Checking the folder ${INITIAL_FOLDER_PATH} existence... ---`);
-    fs.readdir(INITIAL_FOLDER_PATH, (err) => {
-        if(err) {
-            addLogToReportFile(`*** ERROR **** The folder ${INITIAL_FOLDER_PATH} doesn\'t exist. Testing is stopped!!! ***`);
-            return false;
+function checkInitialFolderContent() {
+    if(initialFolderContent.length === 0) {
+        addLogToReportFile(`*** ERROR **** The folder ${INITIAL_FOLDER_PATH} is empty. Testing is stopped!!! ***`);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function showInitialFolderContent() {
+    addLogToReportFile(`--- The folder contains the next files: ---`);
+    for(let i = 0, len = initialFolderContent.length; i < len; i++) {
+        addLogToReportFile(`${initialFolderContent[i]}`);
+    }
+}
+
+function searchForStorelineImportFolder() {
+    addLogToReportFile(`--- Searching for ".../StoreLine/Office/Import" folder... ---`);
+    let trueDestFolder;
+    let contentOfDestFolder = fs.readdirSync(DEST_FOLDER_PATH_32);
+    if(contentOfDestFolder.length === 0) {
+        contentOfDestFolder = fs.readdirSync(DEST_FOLDER_PATH_64);
+        if(contentOfDestFolder.length === 0) {
+            addLogToReportFile(`*** ERROR *** The folder ".../StoreLine/Office/Import" was not found ***`);
+            trueDestFolder = '';
         } else {
-            addLogToReportFile(`--- The folder ${INITIAL_FOLDER_PATH} exists... ---`);
-            return true;
-        };
-    });
+            addLogToReportFile(`--- The folder ${DEST_FOLDER_PATH_64} was successfully found... ---`);
+            trueDestFolder = DEST_FOLDER_PATH_64;
+        }
+    } else {
+        addLogToReportFile(`--- The folder ${DEST_FOLDER_PATH_32} was successfully found... ---`);
+        trueDestFolder = DEST_FOLDER_PATH_32;
+    }
+    return trueDestFolder;
 }
 
 module.exports = { validateAnswer, startTesting };
